2e4346f02ee5735245fd587028815330cf4c062a
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2024-05-05 01:42:25 +0300

    troc

--- src/app/component/troc/troc.component.ts
+++ src/app/component/troc/troc.component.ts
@@ -1,10 +1,33 @@
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
+const toLeft = trigger('fadeToLeft', [
+  state(
+    'load',
+    style({
+      transform: 'translateX(0%)',
+    })
+  ),
+  state(
+    'exit',
+    style({
+      transform: 'translateX(-100%)',
+    })
+  ),
+  transition('* => *', animate('0.3s ease')),
+]);
 @Component({
   selector: 'app-troc',
   templateUrl: './troc.component.html',
-  styleUrl: './troc.component.css'
+  styleUrl: './troc.component.css',
+  animations: [toLeft],
 })
 export class TrocComponent {
-
+  isPageOne: boolean = true;
 }
