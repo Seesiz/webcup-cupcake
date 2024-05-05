bb5bdcb0d7c9ebcaf42c10ea74544f62041bf06f
RAKOTONANAHARY.Mialinirina <mialinirina123@gmail.com>
2024-05-05 09:37:16 +0300

    troc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,5 @@ export class AppComponent {}
 
 //export const baseUrl = 'https://cupcake.madagascar.webcup.hodi.host/backend';
 // export const baseUrl = 'http://localhost:3000';
-export const baseUrl = 'http://10.200.215.76:3000';
-export const baseSocket = 'ws://10.200.215.76:8080';
+export const baseUrl = 'http://10.200.215.74:3000';
+export const baseSocket = 'ws://10.200.215.74:8080';
